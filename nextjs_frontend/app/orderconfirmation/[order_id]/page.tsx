c2ed27f947b38fdd5655e892af78be382cf5ad15
razapanjwani <razapanjwani16986@gmail.com>
2024-09-18 23:00:22 +0500

    added a link

--- nextjs_frontend/app/orderconfirmation/[order_id]/page.tsx
+++ nextjs_frontend/app/orderconfirmation/[order_id]/page.tsx
@@ -133,7 +133,7 @@ const OrderConfirmation = ({ params }: { params: { order_id: number } }) => {
                                     </div>
                                 </div>
                                 <div className="w-full justify-center items-center gap-8 flex sm:flex-row flex-col">
-                                    <Link href={""} className="md:w-fit w-full px-5 py-2.5 bg-indigo-50 hover:bg-indigo-100 transition-all duration-700 ease-in-out rounded-xl justify-center items-center flex">
+                                    <Link href={"/allproducts"} className="md:w-fit w-full px-5 py-2.5 bg-indigo-50 hover:bg-indigo-100 transition-all duration-700 ease-in-out rounded-xl justify-center items-center flex">
                                         <span className="px-2 py-px text-indigo-600 text-base font-semibold leading-relaxed">Back to Shopping</span>
                                     </Link>
                                 </div>
